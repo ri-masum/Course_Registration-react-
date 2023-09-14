59b5893f085f5ba8f3b1cb906ddf849da862ed14
masumahemd <rimasum01724@gmail.com>
2023-09-15 00:37:18 +0600

    the last part of the assignment was to add the total price but this was optionla but i did it anyway .                     so i do this just like i did with credit hour ....declear a variable which will contian recent price the useing useState hook to set the value and pass the data to Cart Section and then receive the data and set the data to the place where it should be shown

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,32 +1,38 @@
-const Cart = ({ names,credts,remaining }) => {
-  console.log(names);
+import PropTypes from "prop-types";
 
+const Cart = ({ names, credts, remaining,totalPrice }) => {
   return (
     <div>
-      <div className="bg-gray-200 w-auto space-y-3">
-       
-       <h1 className="text-sky-500 font-semibold">
+      <div className="bg-gray-200 p-4 rounded-lg w-auto space-y-3">
+        <h1 className="text-sky-500 font-semibold">
           Credit Hour Remaining {remaining} hr
         </h1>
-       
-        
-        <h1 className="text-black font-semibold">Course Name</h1>
 
-        <div>
-          {names.map((name, idx) => (
-            <small key={idx} className="text-start">
-              <li>{name.name}</li>
-            </small>
-          ))}
+        <div className="border border-b-gray-50 border-t-gray-50">
+          <h1 className="text-black font-semibold">Course Name</h1>
+
+          <div className=" p-2">
+            {names.map((name, idx) => (
+              <small key={idx} className="text-start">
+                <li>{name.name}</li>
+              </small>
+            ))}
+          </div>
         </div>
 
         <hr />
         <p className="text-lg">Total Credit Hour :{credts} </p>
         <hr />
-        <h1 className="text-lg">Total Price: USD</h1>
+        <h1 className="text-lg">Total Price:{totalPrice} USD</h1>
       </div>
     </div>
   );
 };
 
+Cart.propTypes = {
+  names: PropTypes.array,
+  credts: PropTypes.number,
+  remaining: PropTypes.number,
+  totalPrice:PropTypes.number
+};
 export default Cart;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,9 @@ const Home = () => {
   const [courses, setCourses] = useState([]);
   const [names,setNames]=useState([])
   const [credts,setCredits]=useState(0)
-  const [remaining,setRemaining]=useState(0)
+  const [remaining,setRemaining]=useState(20)
+  const [totalPrice,setTotalPrice]=useState(0)
+
   useEffect(() => {
     fetch("data.json")
       .then((res) => res.json())
@@ -18,8 +20,11 @@ const Home = () => {
 
     const newNames=[...names,name]
 
+
+
     const isExist=names.find(item=>item.credit ===name.credit)
     let count=name.credit;
+    let price=name.price
     if(isExist){
         return alert('eita already add ace')
 
@@ -31,6 +36,9 @@ const Home = () => {
             count=count+item.credit;
         })
         // console.log(count);
+        names.forEach(item=>{
+            price=price+item.price
+        })
 
         const totalRemaining=20-count;
         if(count>20){
@@ -38,9 +46,13 @@ const Home = () => {
         }
 
         else{
+
+
             setRemaining(totalRemaining)
             setCredits(count)
             setNames(newNames)
+            setTotalPrice(price)
+
           }
 
         }
@@ -52,7 +64,7 @@ const Home = () => {
   return (
     <div>
        <div className="flex gap-5"> 
-       <div className="flex flex-wrap gap-4  bg-red-100 w-4/5">
+       <div className="flex flex-wrap gap-4  w-4/5">
        {
             courses.map((course,idx)=><Cards course={course} key={idx} 
             courses={courses}
@@ -64,6 +76,8 @@ const Home = () => {
         <Cart names={names}
         credts={credts}
         remaining={remaining}
+        totalPrice={totalPrice}
+
         ></Cart>
        </div>
 
